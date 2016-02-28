699f474bf612fbb08d3a1139447a370679c54de9
Brian Boyko <brian.boyko@gmail.com>
2016-02-28 01:15:30 -0600

    Think I'm getting it now

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,20 @@
 const BOOK_SELECTED = 'BOOK_SELECTED'
+const ADD_PAGE = 'ADD_PAGE'
 
 export function selectBook(book){
   // selectBook is an ActionCreator, it needs to return an action
   // an object with a type property. 
   return {
     type: BOOK_SELECTED,
+    payload: book
+  }
+}
+
+export function addPage(book){
+  // selectBook is an ActionCreator, it needs to return an action
+  // an object with a type property. 
+  return {
+    type: ADD_PAGE,
     payload: book
   }
 }
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,28 @@
-export default function (){
-  return [
+const initialState = [
     { title: 'Javascript: The Good Parts', pages:101},
     { title: 'Harry Potter', pages:103},
     { title: 'The Dark Tower', pages:105},
     { title: 'Eloquent Ruby', pages:107}
-   ]
+   ];
+
+
+export default function (state = initialState, action){
+  switch (action.type){
+    case 'ADD_PAGE':
+      let title = action.payload.title
+      var entry;
+      for (var i = 0; i < state.length; i++){
+        if (title === state[i].title){
+          entry = {title: state[i].title, pages: state[i].pages + 1}
+          var newState = state.slice(0, i).concat(entry, state.slice(i+1))
+          console.log(newState); 
+
+          // the reason this isn't updating right away is that the element still has the old object "book" passed in as it's prop. 
+          // you'd have to refactor so that instead of an array, initialState is a map of objects with unique properties. 
+        }
+      }
+      return newState
+    default: 
+      return state;  
+  }
 }
